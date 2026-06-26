6dc534aecb7d09374e1fa77b7c7128c7b03d910a
misoson <sonmisoson@gmail.com>
2026-06-27 04:10:49 +0900

    test

--- src/pe.v
+++ src/pe.v
@@ -1,3 +1,22 @@
-module pe();
+module pe (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire valid_in,
+
+    input  wire mode_int4,
+
+    input  wire signed [7:0] p0, p1, p2,
+    input  wire signed [7:0] p3, p4, p5,
+    input  wire signed [7:0] p6, p7, p8,
+
+    input  wire signed [7:0] w0, w1, w2,
+    input  wire signed [7:0] w3, w4, w5,
+    input  wire signed [7:0] w6, w7, w8,
+
+    input  wire signed [31:0] bias,
+
+    output reg  signed [31:0] conv_out,
+    output reg  valid_out
+);
 
 endmodule
