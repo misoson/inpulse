6c099633f42db665ea47e6600e60a4ab39e573ea
del051028-rgb <del051028@ewha.ac.kr>
2026-07-11 10:40:45 +0900

    first

---
+++ WeightBuffer.srcs/sources_1/new/weight_buffer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/07/11 10:35:24
+// Design Name: 
+// Module Name: weight_buffer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module weight_buffer(
+
+    );
+endmodule
